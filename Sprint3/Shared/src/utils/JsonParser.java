6d89a960b0f7b7194ece6fe8fa177b0843d09b7a
Youssef Topaji <youssefftopaji@gmail.com>
2025-05-24 14:32:27 +0200

    Working on client side

--- Sprint3/Shared/src/utils/JsonParser.java
+++ Sprint3/Shared/src/utils/JsonParser.java
@@ -13,10 +13,6 @@ public class JsonParser
 {
   private static final Gson gson = new Gson();
 
-  public static <T> T jsonToObject(String json, Type type) {
-    return gson.fromJson(json, type);
-  }
-
   /**
    * Converts a User object to JSON
    * @param object The user to convert
@@ -48,101 +44,6 @@ public class JsonParser
   public static <T> List<T> toList(Object payload, Class<T[]> arrayClass) {
     String json = toJson(payload);
     return jsonToList(json, arrayClass);
-  }
-
-  public static String bookingToJson(Booking booking)
-  {
-    Gson gson = new Gson();
-    return gson.toJson(booking);
-  }
-
-  public static Booking jsonToBooking(String jsonResponse)
-  {
-    Gson gson = new Gson();
-    return gson.fromJson(jsonResponse, Booking.class);
-  }
-
-
-
-  /**
-   * Converts JSON to a User object
-   * @param json The JSON string
-   * @return User object
-   */
-  public static User jsonToUser(String json)
-  {
-    Gson gson = new Gson();
-    return gson.fromJson(json, User.class);
-  }
-
-  /**
-   * Converts a LoginRequest object to JSON
-   * @param loginRequest The login request to convert
-   * @return JSON string representation of the login request
-   */
-  public static String toJson(LoginRequest loginRequest)
-  {
-    Gson gson = new Gson();
-    return gson.toJson(loginRequest);
-  }
-
-  /**
-   * Converts JSON to a LoginRequest object
-   * @param request The JSON string
-   * @return LoginRequest object
-   */
-  public static LoginRequest jsonToLoginRequest(String request)
-  {
-    Gson gson = new Gson();
-    return gson.fromJson(request, LoginRequest.class);
-  }
-
-  /**
-   * Converts a list of users to JSON
-   * @param users The list of users to convert
-   * @return JSON string representation of the user list
-   */
-  public static String userListToJson(List<User> users)
-  {
-    Gson gson = new Gson();
-    return gson.toJson(users);
-  }
-
-  /**
-   * Converts JSON to a list of User objects
-   * @param json The JSON string
-   * @return List of User objects
-   */
-  public static List<User> jsonToUserList(String json)
-  {
-    Gson gson = new Gson();
-    User[] userArray = gson.fromJson(json, User[].class);
-    List<User> userList = new ArrayList<>();
-    if (userArray != null) {
-      for (User user : userArray) {
-        userList.add(user);
-      }
-    }
-    return userList;
-  }
-
-  public static String toJson(ArrayList<BookingHistory> bookingHistory)
-  {
-    Gson gson = new Gson();
-    return gson.toJson(bookingHistory);
-  }
-
-  public static List<BookingHistory> jsonToBookingHistory(String jsonResponse)
-  {
-    Gson gson = new Gson();
-    BookingHistory[] bookingHistoryArray = gson.fromJson(jsonResponse, BookingHistory[].class);
-    List<BookingHistory> bookingHistoryList = new ArrayList<>();
-    if (bookingHistoryArray != null) {
-      for (BookingHistory bookingHistory : bookingHistoryArray) {
-        bookingHistoryList.add(bookingHistory);
-      }
-    }
-    return bookingHistoryList;
   }
 
   public static Object jsonToObject(String json, Class<?> clazz)
